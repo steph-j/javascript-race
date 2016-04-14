3fa09b785ebc108f7b322eb8d38f9b291efdcc63
Dev <sjennings.inc@gmail.com>
2016-04-14 13:42:49 +1200

    Add in pics and css table

---
+++ racer.js
@@ -0,0 +1,7 @@
+var stripOne = document.getElementById('player1_strip');
+var stripTwo = document.getElementById('player2_strip');
+
+
+stripOne.addEventListener("click", function() {
+  alert("Start your engins")
+});
