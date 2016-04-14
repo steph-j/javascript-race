0b544821b9a9984017d758ecfcf4e742a9755ecc
Dev <sjennings.inc@gmail.com>
2016-04-14 23:22:15 +1200

    define var

--- racer.js
+++ racer.js
@@ -1,7 +1,5 @@
-var stripOne = document.getElementById('player1_strip');
-var stripTwo = document.getElementById('player2_strip');
-
-
-stripOne.addEventListener("click", function() {
-  alert("Start your engins")
-});
+var strip1 = document.getElementById('player1_strip');
+var strip2 = document.getElementById('player2_strip');
+var toy = document.getElementsByClassName('active');
+var name = prompt("What is your name?"); 
+alert(name + " you're a winner");
